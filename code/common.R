d57734580246282a92cc25819de1ca0dd2e87ce2
Mason Garrison <garrissm@wfu.edu>
2024-05-28 11:08:21 -0400

    updating

--- code/common.R
+++ code/common.R
@@ -1,71 +1,54 @@
-## seeded with content from same in adv-r
-## deleted bits that seem irrelevant
-## commented out bits that look like they may become relevant
-
-
-
 ## Standard Libraries
-if (!require("tweetrmd"))  devtools::install_github("gadenbuie/tweetrmd")
-library(tweetrmd) #... embedding tweets
-library(vembedr)  #... embedding youtube videos
+library(tweetrmd) # Embedding tweets
+library(vembedr) # Embedding YouTube videos
 library(knitr)
+library(webshot)
+library(tidyverse)
+library(htmlwidgets)
 
-## Options
+## Options for knitr chunks
 knitr::opts_chunk$set(
   comment = "#>",
+  error = TRUE,
   collapse = TRUE
-  # cache = TRUE,
-  # fig.retina = 0.8, # figures are either vectors or 300 dpi diagrams
-  # dpi = 300,
-  # out.width = "70%",
-  # fig.align = 'center',
-  # fig.width = 6,
-  # fig.asp = 0.618,  # 1 / phi
-  # fig.show = "hold"
+  # Additional options can be included as needed
 )
 
+## Global options
 options(
   rlang_trace_top_env = rlang::current_env(),
-  rlang__backtrace_on_error = "none"
-)
-
-options(
+  rlang__backtrace_on_error = "none",
   digits = 3,
   str = strOptions(strict.width = "cut")
 )
 
+## Additional options for LaTeX output
 if (knitr::is_latex_output()) {
   knitr::opts_chunk$set(width = 69)
-  options(width = 69)
-  options(crayon.enabled = FALSE)
-  options(cli.unicode = TRUE)
+  options(width = 69, crayon.enabled = FALSE, cli.unicode = TRUE)
 }
 
-# knitr::knit_hooks$set(
-#   small_mar = function(before, options, envir) {
-#     if (before) {
-#       par(mar = c(4.1, 4.1, 0.5, 0.5))
-#     }
-#   }
-# )
-
-
+## Knit hooks
 knitr::knit_hooks$set(chunk_envvar = function(before, options, envir) {
   envvar <- options$chunk_envvar
   if (before && !is.null(envvar)) {
     old_envvar <<- Sys.getenv(names(envvar), names = TRUE, unset = NA)
     do.call("Sys.setenv", as.list(envvar))
-    #print(str(options))
   } else {
     do.call("Sys.setenv", as.list(old_envvar))
   }
 })
 
+## Reading datasets
+ds4p_funyoutube <- read.csv("admin/csv/ds4p_funyoutube.csv", sep = "")
+ds4p_urls <- read.csv("./admin/csv/ds4p_urls.csv")
+
+## Functions
+
+# Check and install packages quietly
 check_quietly <- purrr::quietly(devtools::check)
 install_quietly <- purrr::quietly(devtools::install)
 
-
-# functions
 shhh_check <- function(..., quiet = TRUE) {
   out <- check_quietly(..., quiet = quiet)
   out$result
@@ -78,18 +61,39 @@ pretty_install <- function(...) {
   c(output, out$messages)
 }
 
-
+# Function to handle single or multiple samples
 sample_no_surprises <- function(x) {
   if (length(x) <= 1) {
     return(x)
   } else {
-    return(sample(x,1))
+    return(sample(x, 1))
   }
 }
 
-
-slide_url=function(df_url,title,slide=NULL){
-  var_url=paste0(df_url$link[df_url$title==title],slide)
+# Function to construct slide URLs
+slide_url <- function(df_url, title, slide = NULL) {
+  var_url <- paste0(df_url$link[df_url$title == title], slide)
   return(var_url)
 }
 
+# Function to try including a tweet
+try_include_tweet <- function(tweet_url, plain = FALSE, ...) {
+  return(try(tweetrmd::include_tweet(tweet_url = tweet_url, plain = plain, ...),
+             silent = TRUE))
+}
+
+
+library(stringr)
+embed_youtube_alt <- function(youtube_id) {
+  if (knitr::is_html_output(excludes = "epub")) {
+    url <- str_c("https://www.youtube.com/embed/", youtube_id)
+    return(knitr::include_url(url))
+  } else {
+    # Download thumbnail and use that
+    dir_path <- 'img/youtube'
+    if (!dir.exists(dir_path)) dir.create(dir_path)
+    file_path <- str_c(dir_path, '/', youtube_id, '.jpg')
+    if (!file.exists(file_path)) webshot(str_c("https://img.youtube.com/vi/", youtube_id, "/mqdefault.jpg"),vwidth = 320,vheight=180, file = file_path)
+    return(knitr::include_graphics(str_c(file_path)))
+  }
+}
